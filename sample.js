891a9622c6d3b5017ddf7e6ef14c557abec9c3de
naoyasakua <4869ns@gmail.com>
2020-12-18 13:44:18 +0900

    ボタンクリックでID、名前、コース、受講期が追加されるように編集

---
+++ sample.js
@@ -0,0 +1,27 @@
+new Vue({
+  
+  el: '#app',
+  // dataは、定義されたVue.jsのインスタンスが持つ属性（値）
+  data: {
+    name: 'A山B郎',
+    course: 'Webエンジニアコース',
+    acceptancePeriod: '2019年01月期',
+    defaultLastId: 3,
+    students: [
+      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
+      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
+      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
+    ]
+  },
+  // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
+  methods: {
+    addStudent: function(event) {
+      this.student = this.students.push({
+        id: this.defaultLastId += 1,
+        name: this.name,
+        course: this.course,
+        acceptancePeriod: this.acceptancePeriod
+      })
+    }
+  }
+})
